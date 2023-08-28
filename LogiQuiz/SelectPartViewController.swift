2408bd5c4999c0d32a490c30a068a0a609db1738
Fujiken0705 <kentofujita.new@gmail.com>
2023-08-28 23:43:59 +0900

    問題文データの保存まで完了

--- LogiQuiz/SelectPartViewController.swift
+++ LogiQuiz/SelectPartViewController.swift
@@ -41,7 +41,33 @@ final class SelectPartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // アプリのメインバンドル内のすべてのCSVファイルのパスを取得して印刷
+        let allCSVPaths = Bundle.main.paths(forResourcesOfType: "csv", inDirectory: nil)
+        print("All CSV Paths in the main bundle: \(allCSVPaths)")
+
+        // 特定のリソース名に対して期待されるファイルパスを取得して印刷
+        let resourceNames = ["Quiz1", "Quiz2", "Quiz3"]
+        for resourceName in resourceNames {
+            if let filePath = Bundle.main.path(forResource: resourceName, ofType: "csv") {
+                print("File path for \(resourceName): \(filePath)")
+            } else {
+                print("Failed to find file path for \(resourceName)")
+            }
+        }
+
+        // 保存されているWrongQuizオブジェクトを表示
+        let wrongQuizzes = fetchWrongQuizzes()
+        print("Stored Wrong Quiz IDs: \(wrongQuizzes.map { $0.quizid })")
+
+        // 特定の問題ID（例: "p1q3"）を使用して、fetchQuizメソッドをテスト
+        if let testQuiz = fetchQuiz(from: "p1q3") {
+            print("Fetched quiz for p1q3: \(testQuiz.title)")
+        } else {
+            print("Failed to fetch quiz for p1q3")
+        }
     }
+
 
     @IBAction private func PartButtonAction(sender:UIButton) {
         print(sender.tag)
@@ -58,23 +84,39 @@ final class SelectPartViewController: UIViewController {
     }
 
     func fetchQuiz(from quizId: String) -> Quiz? {
+        print("Fetching quiz for ID: \(quizId)")
+
         let parts = quizId.split(separator: "q")
         guard let part = parts.first, let index = Int(parts.last ?? "") else {
+            print("Failed to split quizId: \(quizId) into part and index")
             return nil
         }
 
-        let filePath = Bundle.main.path(forResource: "Quiz\(part.dropFirst())", ofType: "csv")
+        let resourceName = "Quiz\(part.dropFirst())"
+        guard let filePath = Bundle.main.path(forResource: resourceName, ofType: "csv") else {
+            print("Failed to find CSV for resource name: \(resourceName)")
+            return nil
+        }
+
         do {
-            let csvData = try String(contentsOfFile: filePath!)
+            let csvData = try String(contentsOfFile: filePath)
             let csvLines = csvData.components(separatedBy: .newlines)
+            guard index <= csvLines.count else {
+                print("Index out of range for quizId: \(quizId)")
+                return nil
+            }
             let line = csvLines[index - 1]
             let components = line.components(separatedBy: ",")
+            guard components.count >= 3 else {
+                print("Insufficient data for quizId: \(quizId)")
+                return nil
+            }
             let title = components[0]
             let correctIndex = (Int(components[1]) ?? 1) - 1
             let selections = Array(components[2...])
             return Quiz(id: quizId, title: title, selections: selections, correctIndex: correctIndex)
         } catch {
-            print("Error reading CSV for quiz id: \(quizId)")
+            print("Error reading CSV for quiz id: \(quizId), error: \(error)")
             return nil
         }
     }
@@ -82,28 +124,28 @@ final class SelectPartViewController: UIViewController {
 
     @IBAction func reviewButtonTapped(_ sender: Any) {
         let wrongQuizzes = fetchWrongQuizzes()
+        print("Wrong Quiz IDs: \(wrongQuizzes.map { $0.quizid })")
 
         // 間違えた問題のIDの配列を作成
         let wrongQuizIds = wrongQuizzes.map { $0.quizid }
+
+        // 各間違ったクイズIDに対して、クイズ情報を取得してみる
+        for quizId in wrongQuizIds {
+            if let quiz = fetchQuiz(from: quizId) {
+                print("Successfully fetched quiz for ID \(quizId): \(quiz.title)")
+            } else {
+                print("Failed to fetch quiz for ID \(quizId)")
+            }
+        }
 
         // 間違えた問題を表示する`QuizViewController`を準備
         let quizViewController = QuizViewController(nibName: "QuizViewController", bundle: nil)
 
         // 間違えた問題のIDの配列をもとに`QuizViewModel`を初期化
-        quizViewController.viewModel = QuizViewModel(selectPart: 0, specificQuizIds: wrongQuizIds) // selectPartは0としておきます。specificQuizIdsのみを使用するためです。
+        quizViewController.viewModel = QuizViewModel(selectPart: 0, specificQuizIds: wrongQuizIds)
 
         // 間違えた問題を表示する`QuizViewController`をpush
         navigationController?.pushViewController(quizViewController, animated: true)
     }
-
-    /*
-     // MARK: - Navigation
-
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
 
 }
